0bfdd68f412238091eb8d1bc7bf171d8a383237b
unknown <dangkhoa141003@gmail.com>
2024-03-26 00:47:51 +0700

    init fontend

---
+++ history-site-fontend/src/App.js
@@ -0,0 +1,13 @@
+import AppRoutes from "./components/Routes";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppRoutes />
+      </header>
+    </div>
+  );
+}
+
+export default App;
